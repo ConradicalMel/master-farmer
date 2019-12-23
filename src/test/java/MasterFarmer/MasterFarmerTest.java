0836763af9e9786f8b5225c7f576e8a2594084b8
ConradMelcher <32449783+ConradMelcher@users.noreply.github.com>
2019-12-23 18:20:05 -0500

    Removed exmples, added MasterFarmer plugin

--- src/test/java/MasterFarmer/MasterFarmerTest.java
+++ src/test/java/MasterFarmer/MasterFarmerTest.java
@@ -1,13 +1,13 @@
-package com.example;
+package MasterFarmer;
 
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class ExamplePluginTest
+public class MasterFarmerTest
 {
 	public static void main(String[] args) throws Exception
 	{
-		ExternalPluginManager.loadBuiltin(ExamplePlugin.class);
+		ExternalPluginManager.loadBuiltin(MasterFarmerPlugin.class);
 		RuneLite.main(args);
 	}
 }
